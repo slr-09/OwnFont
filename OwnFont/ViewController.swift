b5323215f3cd1a74011e6144bdb61d1331651124
slr-09 <ge12090@naver.com>
2026-04-09 01:19:38 +0900

    initial commit

---
+++ OwnFont/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  OwnFont
+//
+//  Created by 가은 on 4/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
