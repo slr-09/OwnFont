f65d868edd7c5610b5cb553b9a63392ca6fdb72d
slr-09 <ge12090@naver.com>
2026-04-21 16:29:41 +0900

    fix: firebase analytics 재설정

--- OwnFont/AppDelegate.swift
+++ OwnFont/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseAnalytics
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         CGPathTransformer.register()
         
         FirebaseApp.configure()
+        Analytics.setAnalyticsCollectionEnabled(true)
         
         return true
     }
